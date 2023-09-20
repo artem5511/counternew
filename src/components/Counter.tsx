43ae31cd86a3be5d57fe3b27ba80add18b00e2e0
artem5511 <artem_5511@mail.ru>
2023-09-20 10:32:07 +0300

    init

---
+++ src/components/Counter.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Counter = () => {
+    <div style={{ backgroundColor: "#44014C", width: "300px", minHeight: "200px"}}>
+        <div></div>
+        <div>
+            <div>
+                <button>inc</button>
+            </div>
+            <div>
+                <button>reset</button>
+
+            </div>
+        </div>
+    </div>
+
+}
