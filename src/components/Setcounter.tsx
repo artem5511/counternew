e88f3b52f1360b32b9fd61c610aba6441e50e4fe
artem_ <artem_5511@mail.ru>
2023-10-11 01:54:05 +0300

    commit

--- src/components/Setcounter.tsx
+++ src/components/Setcounter.tsx
@@ -2,20 +2,27 @@ import {Button} from "./Button";
 import e from './setсounter.module.css';
 
 type CounterPropsType = {
-    Inc: () => void
-    Reset: () => void
     // value: number
+    disabled?: boolean
     startValue: number
     maxValue: number
+    setToLocalstorageHandler: () => void
 }
 
 export const Setcounter = (props: CounterPropsType) => {
-
+    const divStyle = {
+       height: '300px'
+    }
     return (
         <div>
             <div className={e.counter}>
-                <div className={e.butmon} onClick={props.Inc}>
-                    <Button name={'SET'} callback={props.Inc} value={999}/>
+                <div className={e.butmon}>
+                    <Button
+                        name={'SET'}
+                        callback={props.setToLocalstorageHandler}
+                        disabled={props.disabled}
+                        value={props.startValue}
+                    />
                 </div>
             </div>
             <div className={e.but}>
